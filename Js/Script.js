fc55403dcb383b4aa21c029f66bd0b604873cc87
pouria <45602285+pouriasadeghi18@users.noreply.github.com>
2021-12-22 16:20:01 +0330

    add owl.carousel add index page

--- Js/Script.js
+++ Js/Script.js
@@ -26,3 +26,48 @@ jQuery(function ($) {
    });
 
 });
+$('.owl-carousel').owlCarousel({
+    items:2,
+
+    rtl:true,
+    loop:true,
+
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+$('.newprofucts').owlCarousel({
+    items:1,
+
+    loop:true,
+
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
